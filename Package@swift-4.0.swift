89059c0a99f60385f039ced9f0f57a1e928cb443
Aaron Liberatore <aml68@duke.edu>
2017-11-10 12:47:29 -0600

    Update Package@swift-4.0.swift (#6)

--- Package@swift-4.0.swift
+++ Package@swift-4.0.swift
@@ -27,7 +27,7 @@ let package = Package(
       )
     ],
     dependencies: [
-      .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", .upToNextMajor(from: "4.0.0")),
+      .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", .upToNextMajor(from: "5.0.0")),
       .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "1.0.0")),
       .package(url: "https://github.com/behrang/YamlSwift.git", .upToNextMajor(from: "3.0.0")),
       .package(url: "https://github.com/IBM-Swift/Kitura-Request.git", .upToNextMajor(from: "0.0.0"))
